7a246fb67ddca594cee24957c918782783aee5c4
Mohamed Saleem <saleemilahi21@gmail.com>
2024-08-25 21:47:34 +0530

    chore: Set up Winston and Morgan logger

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,29 @@
 import express from "express";
 import cors from "cors"
 import helathcheckRouter from "./routes/healthcheck.routes.js";
+import logger from "./utils/logger.js";
+import morgan from "morgan";
 
 // Initializing the Express application
 const app = express();
+
+// Configuring logger Middleware 
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 // Configuring CORS middleware
 app.use(cors({
