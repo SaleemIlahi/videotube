a76eb7f45f95be1a70b6de0971d14792f225ae67
Mohamed Saleem <saleemilahi21@gmail.com>
2024-08-26 22:26:48 +0530

    feat: Add mongoose hook for hash, compare, generateAccessToken and generateRefreshToken

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -48,5 +50,41 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+// Hashing password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Camparing hash password
+userSchema.method.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Generating Json web token
+userSchema.method.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+    },
+    process.env,
+    ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.method.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env,
+    REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
