a808bb0eb9d8559dbaad912a70c0e1357c976d53
Mohamed Saleem <saleemilahi21@gmail.com>
2024-08-30 22:50:31 +0530

    refactor: Added file fiter and extension in the name

--- backend/src/middlewares/multer.middlewares.js
+++ backend/src/middlewares/multer.middlewares.js
@@ -1,13 +1,37 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/tmep");
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname));
   },
 });
 
-export const upload = multer({ storage: storage });
+// Set up file filter to validate file types
+const fileFilter = function (req, file, cb) {
+  // Accept only specific file types, e.g., images
+  const filetypes = /jpeg|jpg|png|mp4/;
+  // validate MIME
+  const mimetype = filetypes.test(file.mimetype);
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb(
+      new Error(
+        "File upload only supports the following file types: " + filetypes
+      )
+    );
+  }
+};
+
+export const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 2 * 1024 * 1024 },
+});
