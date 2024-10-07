f29bf4f56bbb7b6d74708aa205c5333b878667a1
Mohamed Saleem <saleemilahi21@gmail.com>
2024-10-07 21:15:00 +0530

    feat: Integrate react router dom in App.jsx for navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import S from "./styles/app.module.scss";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className={S.app}>
-      {/* <Login /> */}
-      <Register />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/sign-up" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
