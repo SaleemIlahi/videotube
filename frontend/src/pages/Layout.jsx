f123635b275900a72600c76ffccd6475b4bdadc5
Mohamed Saleem <saleemilahi21@gmail.com>
2024-10-26 17:09:42 +0530

    feat: Add Logout functionallity inside profile menu and style header section

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import S from "../styles/layout.module.scss";
-import { useSelector } from "react-redux";
-import { NavLink, Outlet } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import { logout } from "../utils/api";
+import { LOGOUT } from "../features/authSlice";
 
 const Sidebar = () => {
   const routes = useSelector((state) => state.authReducer.auth?.routes);
@@ -28,13 +30,56 @@ const Sidebar = () => {
   );
 };
 const Layout = () => {
+  const [profileMenuActive, setProfileMenuActive] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userLogout = async () => {
+    const res = await logout();
+    if (res.statusCode === 200) {
+      navigate("/", { replace: true });
+      dispatch(LOGOUT());
+    }
+  };
+
+  const menuList = [
+    {
+      id: "profile",
+      name: "Profile",
+      fun: () => console.log("first"),
+    },
+    {
+      id: "logout",
+      name: "Logout",
+      fun: userLogout,
+    },
+  ];
+
   return (
     <div className={S.layout_cnt}>
       <div className={S.layout_cnt_sidebar}>
         <Sidebar />
       </div>
       <div className={S.layout_cnt_content}>
-        <div className={S.layout_cnt_content_header}></div>
+        <div className={S.layout_cnt_content_header}>
+          <div></div>
+          <div className={S.layout_cnt_content_header_profile}>
+            <div
+              className={S.avatar}
+              onClick={() => setProfileMenuActive((p) => !p)}
+            ></div>
+            <div className={S.menu}>
+              {profileMenuActive && (
+                <ul className={S.list}>
+                  {menuList.map((o) => (
+                    <li key={o.id} onClick={o.fun}>
+                      {o.name}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </div>
+        </div>
         <div className={S.layout_cnt_content_body}>
           <Outlet />
         </div>
