317b8a92d587a1e6aa9468c623e098d67ed2b92a
Mohamed Saleem <saleemilahi21@gmail.com>
2024-10-26 23:55:31 +0530

    feat: Read transcoded .m3u8 file and upload each segments one by one in cloudinary

--- backend/src/controllers/video.controller.js
+++ backend/src/controllers/video.controller.js
@@ -1,16 +1,70 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
-import { exec } from "child_process";
 import { ApiError } from "../utils/apiError.js";
 import { runFFmpeg } from "../utils/videoSegment.js";
+import fs from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { uploadHLSOnCloudinary } from "../utils/cloudinary.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const readM3u8File = async (
+  m3u8Path,
+  transcodedVideo,
+  uploadHlsToCloundinary
+) => {
+  try {
+    const data = await fs.readFile(m3u8Path, "utf8");
+    const path = "/public/segment";
+    const tempPath = "/public/temp";
+    const lines = data.split("\n");
+    let videoDetials = [];
+    let segments = lines
+      .filter((line) => line.endsWith(".ts"))
+      .map((line) => line.trim());
+    segments = [...segments, "index.m3u8"];
+
+    for (const segment of segments) {
+      try {
+        const relativePath = join(
+          __dirname,
+          "..",
+          "..",
+          path,
+          transcodedVideo.folder,
+          segment
+        );
+        const folderPath = `videotube/upload/${transcodedVideo.folder}`;
+        let video = await uploadHlsToCloundinary(relativePath, folderPath);
+        videoDetials.push(video);
+      } catch (error) {
+        new ApiError(500, error.message);
+      }
+    }
+    fs.rmdir(join(__dirname, "..", "..", path, transcodedVideo.folder));
+    console.log(
+      join(__dirname, "..", "..", tempPath, `${transcodedVideo.folder}.mp4`)
+    );
+    fs.unlink(
+      join(__dirname, "..", "..", tempPath, `${transcodedVideo.folder}.mp4`)
+    );
+    console.log(videoDetials);
+  } catch (error) {
+    new ApiError(500, error.message);
+  }
+};
 
 const uploadVideo = asyncHandler(async (req, res) => {
-  console.log(req.files);
   let transcodedVideo = await runFFmpeg(
     req.files?.video?.[0]?.filename,
     "index.m3u8"
   );
-  if (transcodedVideo) {
+  if (transcodedVideo.status) {
+    const m3u8Path = join(__dirname, "..", "..", transcodedVideo.filePath);
+    readM3u8File(m3u8Path, transcodedVideo, uploadHLSOnCloudinary);
+
     res.status(200).json(new ApiResponse(200, transcodedVideo));
   }
 });
