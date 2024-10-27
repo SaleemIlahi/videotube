9d2f6be205a464c04b2270e57dbf3542de2a7579
Mohamed Saleem <saleemilahi21@gmail.com>
2024-10-27 13:00:16 +0530

    feat: Checking auth routes before invoking video controller

--- backend/src/routes/video.routes.js
+++ backend/src/routes/video.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { uploadVideo } from "../controllers/video.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJwt } from "../middlewares/auth.middlewares.js";
 
 const videoRouter = Router();
 
 videoRouter.route("/upload").post(
+  verifyJwt,
   upload.fields([
     {
       name: "video",
