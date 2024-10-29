02131e2529c12d7b40ef62ee5889fa6eac330af7
Mohamed Saleem <saleemilahi21@gmail.com>
2024-10-29 15:19:09 +0530

    chore: Remove try-catch code block

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,77 +1,61 @@
 const baseUrl = "http://localhost:8000/api/v1";
 
 const postMethod = async (props) => {
-  try {
-    const { url, b, header } = props;
-    const res = await fetch(url, {
-      method: "POST",
-      credentials: "include",
-      headers: header
-        ? {
-            "Content-Type": "application/json",
-          }
-        : {},
-      body: b,
-    });
+  const { url, b, header } = props;
+  const res = await fetch(url, {
+    method: "POST",
+    credentials: "include",
+    headers: header
+      ? {
+          "Content-Type": "application/json",
+        }
+      : {},
+    body: b,
+  });
 
-    const output = await res.json();
-    return output;
-  } catch (error) {}
+  const output = await res.json();
+  return output;
 };
 
 const getMethod = async (props) => {
-  try {
-    const { url } = props;
-    const res = await fetch(url, {
-      credentials: "include",
-    });
+  const { url } = props;
+  const res = await fetch(url, {
+    credentials: "include",
+  });
 
-    const output = await res.json();
-    return output;
-  } catch (error) {}
+  const output = await res.json();
+  return output;
 };
 
 export const register = async (b) => {
-  try {
-    const res = await postMethod({
-      url: `${baseUrl}/auth/register`,
-      b,
-      header: false,
-    });
-    return res;
-  } catch (error) {
-    console.log(error);
-  }
+  const res = await postMethod({
+    url: `${baseUrl}/auth/register`,
+    b,
+    header: false,
+  });
+  return res;
 };
 
 export const login = async (b, t) => {
-  try {
-    if (t === "POST") {
-      const res = await postMethod({
-        url: `${baseUrl}/auth/login`,
-        b,
-        header: true,
-      });
-      return res;
-    } else {
-      const res = await getMethod({
-        url: `${baseUrl}/auth/login`,
-      });
-      return res;
-    }
-  } catch (error) {
-    console.log(error);
+  if (t === "POST") {
+    const res = await postMethod({
+      url: `${baseUrl}/auth/login`,
+      b,
+      header: true,
+    });
+    return res;
+  } else {
+    const res = await getMethod({
+      url: `${baseUrl}/auth/login`,
+    });
+    return res;
   }
 };
 
 export const logout = async () => {
-  try {
-    const res = await postMethod({
-      url: `${baseUrl}/auth/logout`,
-      header: true,
-    });
-    return res;
-  } catch (error) {
-    console.log(error);
-  }
+  const res = await postMethod({
+    url: `${baseUrl}/auth/logout`,
+    header: true,
+  });
+  return res;
 };
