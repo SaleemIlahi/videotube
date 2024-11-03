f8849edd76f45a29e1e242ada2e52584d837f31e
Mohamed Saleem <saleemilahi21@gmail.com>
2024-11-03 21:53:49 +0530

    feat: Add thumbnail overlay and stop autoplay

--- frontend/src/components/HlsPlayer.jsx
+++ frontend/src/components/HlsPlayer.jsx
@@ -1,18 +1,21 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Hls from "hls.js";
 import S from "../styles/hls.module.scss";
+import Icons from "./Icons";
 
 function HlsPlayer(props) {
-  const { src, type, controls, autoplay } = props;
+  const { src, type, controls, autoplay, thumbnail } = props;
   const videoRef = useRef(null);
-
+  const [play, setPlay] = useState(autoplay);
   useEffect(() => {
     if (videoRef.current && Hls.isSupported()) {
       const hls = new Hls();
       hls.loadSource(src);
       hls.attachMedia(videoRef.current);
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        videoRef.current.play();
+        if (play) {
+          videoRef.current.play();
+        }
       });
 
       return () => {
@@ -21,12 +24,21 @@ function HlsPlayer(props) {
         }
       };
     }
-  }, [src]);
+  }, [src, play]);
   return (
     <div className={S.hls_player}>
-      <video ref={videoRef} controls={controls} autoplay={autoplay}>
-        <source type={type} src={src} />
-      </video>
+      {play ? (
+        <video ref={videoRef} controls={controls} autoPlay={play}>
+          <source type={type} src={src} />
+        </video>
+      ) : (
+        <div className={S.thumbnail} onClick={() => setPlay(true)}>
+          <img src={thumbnail} alt={thumbnail} />
+          <div className={S.play_overlay}>
+            <Icons name="play" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
